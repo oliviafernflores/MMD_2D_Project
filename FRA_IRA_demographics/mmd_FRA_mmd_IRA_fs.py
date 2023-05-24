4e9b336c432ca492c4c5e83a379cdf8c9ab5a3c5
oliviafernflores <olivia@dhcp-10-132-199-200.uawifi.arizona.edu>
2023-05-24 09:26:27 -0700

    fixing typos

--- FRA_IRA_demographics/mmd_FRA_mmd_IRA_fs.py
+++ FRA_IRA_demographics/mmd_FRA_mmd_IRA_fs.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 dd = pickle.load(open('/groups/rgutenk/mice/mouse_all_pops.dd_syn.bpkl', 'rb'))
 pop_ids = ['Mmd_FRA', 'Mmd_IRA']
 ns = [14, 14]
-fs = dadi.Spectrum_.from_data_dict(dd, pop_ids, ns, polarized = False)
+fs = dadi.Spectrum.from_data_dict(dd, pop_ids, ns, polarized = False)
 plt.figure('IRA_FRA_fs')
 dadi.Plotting.plot_single_2d_sfs(fs)
 plt.savefig('IRA_FRA_fs.png')
