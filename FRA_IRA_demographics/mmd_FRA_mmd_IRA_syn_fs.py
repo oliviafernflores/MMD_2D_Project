2018614a676d1b8d0a44c7047ff69a9b06f6c8b6
oliviafernflores <oliviafernflores@arizona.edu>
2023-05-24 12:19:16 -0700

    adjusting bootstraps and plotting

--- FRA_IRA_demographics/mmd_FRA_mmd_IRA_syn_fs.py
+++ FRA_IRA_demographics/mmd_FRA_mmd_IRA_syn_fs.py
@@ -23,12 +23,18 @@ nboot, chunk_size = 100, 2e6
 chunks = dadi.Misc.fragment_data_dict(dd, chunk_size)
 boots = dadi.Misc.bootstraps_from_dd_chunks(chunks, nboot, pop_ids, ns)
 for i in range(len(boots)):
-    boots[i].to_file('syn_bootstraps/IRA_FRA{0}.fs'.format(str(i)))
+    boots[i].to_file('syn_bootstraps/IRA_FRA_{0}.fs'.format(str(i)))
 
-plt.figure('IRA_FRA_fs')
-dadi.Plotting.plot_single_2d_sfs(fs)
+fig, ax = plt.subplots(3)
+
+dadi.Plotting.plot_single_2d_sfs(fs, ax = ax[0])
+
+dadi.Plotting.plot_single_2d_sfs(boots[0], ax = ax[1])
+
+dadi.Plotting.plot_2d_resid(fs - boots[0], ax = ax[2])
+
+ax[0].set_title('Original SFS')
+ax[1].set_title('Bootstrapped SFS')
+ax[2].set_title('Residuals')
+
 plt.savefig('IRA_FRA_syn_fs.png')
-
-plt.figure('IRA_FRA_fs_bootstraps')
-dadi.Plotting.plot_single_2d_sfs(boots[0])
-plt.savefig('IRA_FRA_syn_bootstraps_fs.png')
