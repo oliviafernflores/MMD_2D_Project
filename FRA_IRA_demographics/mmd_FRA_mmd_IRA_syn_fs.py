419a188b4e6ad07e1fbc26d2514bf19ef6e5f0c8
oliviafernflores <oliviafernflores@arizona.edu>
2023-05-24 12:38:16 -0700

    turning the sfs and bootstrap generation into a function, making it easier to switch between syn and nsyn

--- FRA_IRA_demographics/mmd_FRA_mmd_IRA_syn_fs.py
+++ FRA_IRA_demographics/mmd_FRA_mmd_IRA_syn_fs.py
@@ -13,28 +13,34 @@ import pickle
 import nlopt
 import matplotlib.pyplot as plt
 
-dd = pickle.load(open('/groups/rgutenk/mice/mouse_all_pops.dd_syn.bpkl', 'rb'))
-pop_ids = ['Mmd_FRA', 'Mmd_IRA']
-ns = [14, 14]
-fs = dadi.Spectrum.from_data_dict(dd, pop_ids, ns)
-fs.to_file('IRA_FRA_syn_unfolded.fs')
+def fs_and_boostraps(kind):
+    dd = pickle.load(open('/groups/rgutenk/mice/mouse_all_pops.dd_' + kind + '.bpkl', 'rb'))
+    pop_ids = ['Mmd_FRA', 'Mmd_IRA']
+    ns = [14, 14]
+    fs = dadi.Spectrum.from_data_dict(dd, pop_ids, ns)
+    fs.to_file('IRA_FRA_' + kind + '_unfolded.fs')
 
-nboot, chunk_size = 100, 2e6
-chunks = dadi.Misc.fragment_data_dict(dd, chunk_size)
-boots = dadi.Misc.bootstraps_from_dd_chunks(chunks, nboot, pop_ids, ns)
-for i in range(len(boots)):
-    boots[i].to_file('syn_bootstraps/IRA_FRA_{0}.fs'.format(str(i)))
+    nboot, chunk_size = 100, 2e6
+    chunks = dadi.Misc.fragment_data_dict(dd, chunk_size)
+    boots = dadi.Misc.bootstraps_from_dd_chunks(chunks, nboot, pop_ids, ns)
+    for i in range(len(boots)):
+        boots[i].to_file(kind + '_bootstraps/IRA_FRA_{0}.fs'.format(str(i)))
 
-fig = plt.figure(1, figsize = (10,6))
-fig.clear()
-ax = fig.add_subplot(2, 3, 1)
-ax.set_title('Original SFS')
-dadi.Plotting.plot_single_2d_sfs(fs, ax = ax)
-ax = fig.add_subplot(2, 3, 3)
-ax.set_title('Bootstrapped SFS')
-dadi.Plotting.plot_single_2d_sfs(boots[0], ax = ax)
-ax = fig.add_subplot(2, 3, 5)
-ax.set_title('Residuals')
-dadi.Plotting.plot_2d_resid(fs - boots[0])
+    fig = plt.figure(1, figsize = (10,6))
+    fig.clear()
+    ax = fig.add_subplot(2, 3, 1)
+    ax.set_title('Original SFS')
+    dadi.Plotting.plot_single_2d_sfs(fs, ax = ax)
+    ax = fig.add_subplot(2, 3, 3)
+    ax.set_title('Bootstrapped SFS')
+    dadi.Plotting.plot_single_2d_sfs(boots[0], ax = ax)
+    ax = fig.add_subplot(2, 3, 5)
+    ax.set_title('Residuals')
+    dadi.Plotting.plot_2d_resid(fs - boots[0])
 
-plt.savefig('IRA_FRA_syn_fs.png')
+    plt.savefig('IRA_FRA_' + kind + '_fs.png')
+def main():
+    fs_and_boostraps('syn')
+    fs_and_boostraps('nsyn')
+if __name__ == '__main__':
+    main()
