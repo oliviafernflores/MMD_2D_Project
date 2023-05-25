1a6a3f6691b70720d9b5d09e8fa4773203aea9b4
oliviafernflores <oliviafernflores@arizona.edu>
2023-05-25 14:25:42 -0700

    adding plotting for im demography model

--- FRA_IRA_demographics/mmd_FRA_mmd_IRA_plot_demographics.py
+++ FRA_IRA_demographics/mmd_FRA_mmd_IRA_plot_demographics.py
@@ -5,12 +5,18 @@ import csv
 
 def get_popt(fname):
     popt = []
-    with open(fname, newline = '') as fits_file:
-        fits = csv.reader(fits_file, delimiter = '\t')
-        popt = fits[0]
-        for fit in fits:
-            if fit[0] > popt[0]:
-                popt = fit
+    fits_file = open(fname, 'r')
+    fits = fits_file.readlines()
+    popt = fits[0]
+    for f in fits:
+        if f[0] > popt[0]:
+            popt = f
+    popt = popt.split('\t')
+    popt.pop(0)
+    popt.pop(-1)
+    for i in range(len(popt)):
+        popt[i] = float(popt[i])
+    print(popt)
     return popt
 
 def plot_im_demography(fs, ns, pts, popt):
@@ -21,12 +27,23 @@ def plot_im_demography(fs, ns, pts, popt):
     fig = plt.figure('IRA_FRA_IM_Demography')
     fig.clear()
     dadi.Plotting.plot_2d_comp_multinom(model, fs)
-    fig.savefig('IRA_FRA_im_demography.png')
+    fig.savefig('plots/IRA_FRA_im_demography.png')
+def plot_im_pre_demography(fs, ns, pts, popt):
+    demo_model = dadi.Demographics2D.IM_pre
+    demo_model = dadi.Numerics.make_anc_state_misid_func(demo_model)
+    demo_model_ex = dadi.Numerics.make_extrap_func(demo_model)
+    model = demo_model_ex(popt, ns, pts)
+    fig = plt.figure('IRA_FRA_IM_pre_Demography')
+    fig.clear()
+    dadi.Plotting.plot_2d_comp_multinom(model, fs)
+    fig.savefig('plots/IRA_FRA_im_pre_demography.png')
 def main():
     fs = dadi.Spectrum.from_file('IRA_FRA_syn_unfolded.fs')
     ns = fs.sample_sizes
     pts_l = [max(ns)+20, max(ns)+30, max(ns)+40]
+
+    #im_demography
     im_popt = get_popt('results/IRA_FRA_im_demo_fits_combined.txt')
-    print(im_popt)
+    plot_im_demography(fs, ns, pts_l, im_popt)
 if __name__ == '__main__':
     main()
