844b2495041352f5bfd7bd66043ab184e5c09d89
oliviafernflores <oliviafernflores@arizona.edu>
2023-05-25 13:10:53 -0700

    functions for all demographic models

--- FRA_IRA_demographics/mmd_FRA_mmd_IRA_2d_demographics.py
+++ FRA_IRA_demographics/mmd_FRA_mmd_IRA_2d_demographics.py
@@ -40,21 +40,174 @@ def im_demography(fs, ns, pts):
         fid = open(f'demo_results/IRA_FRA_im_demo_fits{process_ii}.txt', 'w')
     for i in range(20):
         p0 = dadi.Misc.perturb_params(params, fold = 0, upper_bound = upper, lower_bound = lower)
-        print('Beginning optimization ' + str(i) + '*'*20)
+        print('Beginning im optimization ' + str(i) + '*'*20)
         popt, ll_model = dadi.Inference.opt(p0, fs, demo_model_ex, pts, upper_bound = upper, lower_bound = lower)
         model_fs = demo_model_ex(popt, ns, pts)
         theta0 = dadi.Inference.optimal_sfs_scaling(model_fs, fs)
         res = [ll_model] + list(popt) + [theta0]
         fid.write('\t'.join([str(ele) for ele in res]) + '\n')
-        print('Finished optimization ' + str(i) + '*'*20)
+        print('Finished im optimization ' + str(i) + '*'*20)
     fid.close()
-
+def im_pre_demography(fs, ns, pts):
+    demo_model = dadi.Demographics2D.IM_pre
+    demo_model = dadi.Numerics.make_anc_state_misid_func(demo_model)
+    demo_model_ex = dadi.Numerics.make_extrap_func(demo_model)
+    params = [1, 0.1, 0.1, 1, 1, 0.1, 0.1, 0.1, 0.1]
+    lower = [1e-2, 1e-3, 1e-3, 1e-2, 1e-2, 1e-3, 1e-3, 1e-3, 0]
+    upper = [3, 1, 1, 3, 3, 1, 1, 1, 1]
+    try:
+        fid = open(f'demo_results/IRA_FRA_im_pre_demo_fits{process_ii}.txt', 'a')
+    except:
+        fid = open(f'demo_results/IRA_FRA_im_pre_demo_fits{process_ii}.txt', 'w')
+    for i in range(20):
+        p0 = dadi.Misc.perturb_params(params, fold = 0, upper_bound = upper, lower_bound = lower)
+        print('Beginning im_pre optimization ' + str(i) + '*'*20)
+        popt, ll_model = dadi.Inference.opt(p0, fs, demo_model_ex, pts, upper_bound = upper, lower_bound = lower)
+        model_fs = demo_model_ex(popt, ns, pts)
+        theta0 = dadi.Inference.optimal_sfs_scaling(model_fs, fs)
+        res = [ll_model] + list(popt) + [theta0]
+        fid.write('\t'.join([str(ele) for ele in res]) + '\n')
+        print('Finished im_pre optimization ' + str(i) + '*'*20)
+    fid.close()
+def bottlegrowth_demography(fs,  ns, pts):
+    demo_model = dadi.Demographics2D.bottlegrowth_2d
+    demo_model = dadi.Numerics.make_anc_state_misid_func(demo_model)
+    demo_model_ex = dadi.Numerics.make_extrap_func(demo_model)
+    params = [1, 1, 0.1, 0.1]
+    lower = [1e-2, 1e-2, 1e-3, 0]
+    upper = [3, 3, 1, 1]
+    try:
+        fid = open(f'demo_results/IRA_FRA_bottlegrowth_demo_fits{process_ii}.txt', 'a')
+    except:
+        fid = open(f'demo_results/IRA_FRA_bottlegrowth_demo_fits{process_ii}.txt', 'w')
+    for i in range(20):
+        p0 = dadi.Misc.perturb_params(params, fold = 0, upper_bound = upper, lower_bound = lower)
+        print('Beginning bottlegrowth optimization ' + str(i) + '*'*20)
+        popt, ll_model = dadi.Inference.opt(p0, fs, demo_model_ex, pts, upper_bound = upper, lower_bound = lower)
+        model_fs = demo_model_ex(popt, ns, pts)
+        theta0 = dadi.Inference.optimal_sfs_scaling(model_fs, fs)
+        res = [ll_model] + list(popt) + [theta0]
+        fid.write('\t'.join([str(ele) for ele in res]) + '\n')
+        print('Finished bottlegrowth optimization ' + str(i) + '*'*20)
+    fid.close()
+def bottlegrowth_split_demography(fs, ns, pts):
+    demo_model = dadi.Demographics2D.bottlegrowth_split
+    demo_model = dadi.Numerics.make_anc_state_misid_func(demo_model)
+    demo_model_ex = dadi.Numerics.make_extrap_func(demo_model)
+    params = [1, 1, 0.1, 0.1, 0.1, 0.1]
+    lower = [1e-2, 1e-2, 1e-3, 1e-3, 1e-3, 0]
+    upper = [3, 3, 1, 1, 1, 1]
+    try:
+        fid = open(f'demo_results/IRA_FRA_bottlegrowth_split_fits{process_ii}.txt', 'a')
+    except:
+        fid = open(f'demo_results/IRA_FRA_bottlegrowth_split_demo_fits{process_ii}.txt', 'w')
+    for i in range(20):
+        p0 = dadi.Misc.perturb_params(params, fold = 0, upper_bound = upper, lower_bound = lower)
+        print('Beginning bottlegrowth_split optimization ' + str(i) + '*'*20)
+        popt, ll_model = dadi.Inference.opt(p0, fs, demo_model_ex, pts, upper_bound = upper, lower_bound = lower)
+        model_fs = demo_model_ex(popt, ns, pts)
+        theta0 = dadi.Inference.optimal_sfs_scaling(model_fs, fs)
+        res = [ll_model] + list(popt) + [theta0]
+        fid.write('\t'.join([str(ele) for ele in res]) + '\n')
+        print('Finished bottlegrowth_split optimization ' + str(i) + '*'*20)
+    fid.close()
+def bottlegrowth_split_mig_demography(fs, ns, pts):
+    demo_model = dadi.Demographics2D.bottlegrowth_split_mig
+    demo_model = dadi.Numerics.make_anc_state_misid_func(demo_model)
+    demo_model_ex = dadi.Numerics.make_extrap_func(demo_model)
+    params = [1, 1, 0.1, 0.1, 0.1, 0.1]
+    lower = [1e-2, 1e-2, 1e-3, 1e-3, 1e-3, 0]
+    upper = [3, 3, 1, 1, 1, 1]
+    try:
+        fid = open(f'demo_results/IRA_FRA_bottlegrowth_split_mig_demo_fits{process_ii}.txt', 'a')
+    except:
+        fid = open(f'demo_results/IRA_FRA_bottlegrwoth_split_mig_demo_fits{process_ii}.txt', 'w')
+    for i in range(20):
+        p0 = dadi.Misc.perturb_params(params, fold = 0, upper_bound = upper, lower_bound = lower)
+        print('Beginning bottlegrowth_split_mig optimization ' + str(i) + '*'*20)
+        popt, ll_model = dadi.Inference.opt(p0, fs, demo_model_ex, pts, upper_bound = upper, lower_bound = lower)
+        model_fs = demo_model_ex(popt, ns, pts)
+        theta0 = dadi.Inference.optimal_sfs_scaling(model_fs, fs)
+        res = [ll_model] + list(popt) + [theta0]
+        fid.write('\t'.join([str(ele) for ele in res]) + '\n')
+        print('Finished bottlegrowth_split_mig optimization ' + str(i) + '*'*20)
+    fid.close()
+def split_asym_mig_demography(fs, ns, pts):
+    demo_model = dadi.Demographics2D.split_asym_mig
+    demo_model = dadi.Numerics.make_anc_state_misid_func(demo_model)
+    demo_model_ex = dadi.Numerics.make_extrap_func(demo_model)
+    params = [1, 1, 0.1, 0.1, 0.1, 0.1]
+    lower = [1e-2, 1e-2, 1e-3, 1e-3, 1e-3, 0]
+    upper = [3, 3, 1, 1, 1, 1]
+    try:
+        fid = open(f'demo_results/IRA_FRA_split_asym_mig_demo_fits{process_ii}.txt', 'a')
+    except:
+        fid = open(f'demo_results/IRA_FRA_split_asym_mig_demo_fits{process_ii}.txt', 'w')
+    for i in range(20):
+        p0 = dadi.Misc.perturb_params(params, fold = 0, upper_bound = upper, lower_bound = lower)
+        print('Beginning split_asym_mig optimization ' + str(i) + '*'*20)
+        popt, ll_model = dadi.Inference.opt(p0, fs, demo_model_ex, pts, upper_bound = upper, lower_bound = lower)
+        model_fs = demo_model_ex(popt, ns, pts)
+        theta0 = dadi.Inference.optimal_sfs_scaling(model_fs, fs)
+        res = [ll_model] + list(popt) + [theta0]
+        fid.write('\t'.join([str(ele) for ele in res]) + '\n')
+        print('Finished split_asym_mig optimization ' + str(i) + '*'*20)
+    fid.close()
+def split_delay_mig_demography(fs, ns, pts):
+    demo_model = dadi.Demographics2D.split_delay_mig
+    demo_model = dadi.Numerics.make_anc_state_misid_func(demo_model)
+    demo_model_ex = dadi.Numerics.make_extrap_func(demo_model)
+    params = [1, 1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+    lower = [1e-2, 1e-2, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 0]
+    upper = [3, 3, 1, 1, 1, 1, 1, 1]
+    try:
+        fid = open(f'demo_results/IRA_FRA_split_delay_mig_demo_fits{process_ii}.txt', 'a')
+    except:
+        fid = open(f'demo_results/IRA_FRA_split_delay_mig_demo_fits{process_ii}.txt', 'w')
+    for i in range(20):
+        p0 = dadi.Misc.perturb_params(params, fold = 0, upper_bound = upper, lower_bound = lower)
+        print('Beginning split_delay_mig optimization ' + str(i) + '*'*20)
+        popt, ll_model = dadi.Inference.opt(p0, fs, demo_model_ex, pts, upper_bound = upper, lower_bound = lower)
+        model_fs = demo_model_ex(popt, ns, pts)
+        theta0 = dadi.Inference.optimal_sfs_scaling(model_fs, fs)
+        res = [ll_model] + list(popt) + [theta0]
+        fid.write('\t'.join([str(ele) for ele in res]) + '\n')
+        print('Finished split_delay_mig optimization ' + str(i) + '*'*20)
+    fid.close()
+def split_mig_demography(fs, ns, pts):
+    demo_model = dadi.Demographics2D.split_mig
+    demo_model = dadi.Numerics.make_anc_state_misid_func(demo_model)
+    demo_model_ex = dadi.Numerics.make_extrap_func(demo_model)
+    params = [1, 1, 0.1, 0.1, 0.1]
+    lower = [1e-2, 1e-2, 1e-3, 1e-3, 0]
+    upper = [3, 3, 1, 1, 1]
+    try:
+        fid = open(f'demo_results/IRA_FRA_split_mig_demo_fits{process_ii}.txt', 'a')
+    except:
+        fid = open(f'demo_results/IRA_FRA_split_mig_demo_fits{process_ii}.txt', 'w')
+    for i in range(20):
+        p0 = dadi.Misc.perturb_params(params, fold = 0, upper_bound = upper, lower_bound = lower)
+        print('Beginning split_mig optimization ' + str(i) + '*'*20)
+        popt, ll_model = dadi.Inference.opt(p0, fs, demo_model_ex, pts, upper_bound = upper, lower_bound = lower)
+        model_fs = demo_model_ex(popt, ns, pts)
+        theta0 = dadi.Inference.optimal_sfs_scaling(model_fs, fs)
+        res = [ll_model] + list(popt) + [theta0]
+        fid.write('\t'.join([str(ele) for ele in res]) + '\n')
+        print('Finished split_mig optimization ' + str(i) + '*'*20)
+    fid.close()
 def main():
     data_fs = dadi.Spectrum.from_file('IRA_FRA_syn_unfolded.fs')
     ns = data_fs.sample_sizes
     pts_l = [max(ns)+20, max(ns)+30, max(ns)+40]
 
     snm_demography(data_fs, ns, 100)
-    im_demography(data_fs, ns, pts_l)
+    # im_demography(data_fs, ns, pts_l)
+    im_pre_demography(data_fs, ns, 100)
+    # bottlegrowth_demography(data_fs, ns, 100)
+    # bottlegrowth_split_demography(data_fs, ns, 100)
+    # bottlegrowth_split_mig_demography(data_fs, ns, 100)
+    # split_asym_mig_demography(data_fs, ns, 100)
+    # split_delay_mig_demography(data_fs, ns, 100)
+    # split_mig_demography(data_fs, ns, 100)
 if __name__ == '__main__':
     main()
