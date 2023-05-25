d817ae5da2b66307c6f11719a62f2f933f97111c
oliviafernflores <oliviafernflores@arizona.edu>
2023-05-25 09:25:42 -0700

    updating HPC info

--- FRA_IRA_demographics/mmd_FRA_mmd_IRA_2d_demographics.py
+++ FRA_IRA_demographics/mmd_FRA_mmd_IRA_2d_demographics.py
@@ -13,6 +13,13 @@ import pickle
 import nlopt
 import matplotlib.pyplot as plt
 
+import sys, os
+print('Script running\n')
+if 'SLURM_SUBMIT_DIR' in os.environ:
+    sys.path.insert(0, os.environ['SLURM_SUBMIT_DIR'])
+process_ii = int(os.environ.get('SLURM_ARRAY_TASK_ID', 1)) - 1
+print(process_ii)
+
 def snm_demography(fs, ns, pts):
     demo_model = dadi.Demographics2D.snm_2d
     model = demo_model([], ns, pts)
@@ -27,16 +34,18 @@ def im_demography(fs, ns, pts):
     lower = [1e-3, 1e-2, 1e-2, 1e-3, 1e-3, 1e-3, 0]
     upper = [1, 3, 3, 1, 1, 1, 1]
     try:
-        fid = open('demo_results/IRA_FRA_im_demo_fits.txt', 'a')
+        fid = open(f'demo_results/IRA_FRA_im_demo_fits{process_ii}.txt', 'a')
     except:
-        fid = open('demo_results/IRA_FRA_im_demo_fits.txt', 'w')
+        fid = open(f'demo_results/IRA_FRA_im_demo_fits{process_ii}.txt', 'w')
     for i in range(20):
         p0 = dadi.Misc.perturb_params(params, fold = 0, upper_bound = upper, lower_bound = lower)
+        print('Beginning optimization ' + str(i) + '*'*20)
         popt, ll_model = dadi.Inference.opt(p0, fs, demo_model_ex, pts, upper_bound = upper, lower_bound = lower)
         model_fs = demo_model_ex(popt, ns, pts)
         theta0 = dadi.Inference.optimal_sfs_scaling(model_fs, fs)
         res = [ll_model] + list(popt) + [theta0]
         fid.write('\t'.join([str(ele) for ele in res]) + '\n')
+        print('Finished optimization ' + str(i) + '*'*20)
     fid.close()
 
 def main():
