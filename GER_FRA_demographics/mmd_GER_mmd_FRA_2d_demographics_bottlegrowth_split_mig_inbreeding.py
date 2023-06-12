8801d5488e99d22fff6ec7ce3642083391292204
oliviafernflores <oliviafernflores@arizona.edu>
2023-06-12 12:18:34 -0700

    inbreeding models for GER and FRA

---
+++ GER_FRA_demographics/mmd_GER_mmd_FRA_2d_demographics_bottlegrowth_split_mig_inbreeding.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+#SBATCH --account=rgutenk
+#SBATCH --qos=user_qos_rgutenk
+#SBATCH --partition=high_priority
+#SBATCH --job-name="FRA_GER_bottlegrowth_split_mig_inbreeding"
+#SBATCH --output=%x-%A_%a.out
+#SBATCH --nodes=1
+#SBATCH --ntasks=50
+#SBATCH --time=24:00:00
+#SBATCH --array=1-5
+from contextlib import AsyncExitStack
+import dadi
+import pickle
+import nlopt
+import matplotlib.pyplot as plt
+import numpy
+from dadi import Numerics, PhiManip, Integration
+from dadi.Spectrum_mod import Spectrum
+
+import sys, os
+print('Script running\n')
+if 'SLURM_SUBMIT_DIR' in os.environ:
+    sys.path.insert(0, os.environ['SLURM_SUBMIT_DIR'])
+process_ii = int(os.environ.get('SLURM_ARRAY_TASK_ID', 1)) - 1
+print(process_ii)
+
+def bottlegrowth_split_mig(params, ns, pts):
+    """
+    params = (nuB,nuF,m,T,Ts)
+    ns = (n1,n2)
+
+    Instantanous size change followed by exponential growth then split with
+    migration.
+
+    nuB: Ratio of population size after instantanous change to ancient
+         population size
+    nuF: Ratio of contempoary to ancient population size
+    m: Migration rate between the two populations (2*Na*m).
+    T: Time in the past at which instantaneous change happened and growth began
+       (in units of 2*Na generations) 
+    Ts: Time in the past at which the two populations split.
+    n1,n2: Sample sizes of resulting Spectrum
+    pts: Number of grid points to use in integration.
+    """
+    nuB,nuF,m,T,Ts, F1, F2 = params
+
+    xx = Numerics.default_grid(pts)
+    phi = PhiManip.phi_1D(xx)
+
+    if T >= Ts:
+        nu_func = lambda t: nuB*numpy.exp(numpy.log(nuF/nuB) * t/T)
+        phi = Integration.one_pop(phi, xx, T-Ts, nu_func)
+
+        phi = PhiManip.phi_1D_to_2D(xx, phi)
+        nu0 = nu_func(T-Ts)
+        nu_func = lambda t: nu0*numpy.exp(numpy.log(nuF/nu0) * t/Ts)
+        phi = Integration.two_pops(phi, xx, Ts, nu_func, nu_func, m12=m, m21=m)
+    else:
+        phi = PhiManip.phi_1D_to_2D(xx, phi)
+        phi = Integration.two_pops(phi, xx, Ts-T, 1, 1, m12=m, m21=m)
+        nu_func = lambda t: nuB*numpy.exp(numpy.log(nuF/nuB) * t/T)
+        phi = Integration.two_pops(phi, xx, T, nu_func, nu_func, m12=m, m21=m)
+
+    fs = Spectrum.from_phi_inbreeding(phi, ns, (xx,xx), (F1, F2), (2, 2))
+    return fs
+
+def bottlegrowth_split_mig_demography(fs, ns, pts):
+    demo_model = bottlegrowth_split_mig
+    demo_model = dadi.Numerics.make_anc_state_misid_func(demo_model)
+    demo_model_ex = dadi.Numerics.make_extrap_func(demo_model)
+    params = [1, 1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+    lower = [1e-2, 1e-2, 1e-3, 1e-3, 1e-3, 1e-3, 0, 0]
+    upper = [3, 10, 1, 1, 3, 1, 1, 1]
+    try:
+        fid = open(f'demo_results/FRA_GER_bottlegrowth_split_mig_inbreeding_demo_fits{process_ii}.txt', 'a')
+    except:
+        fid = open(f'demo_results/FRA_GER_bottlegrowth_split_mig_inbreeding_demo_fits{process_ii}.txt', 'w')
+    for i in range(20):
+        p0 = dadi.Misc.perturb_params(params, fold = 0, upper_bound = upper, lower_bound = lower)
+        print('Beginning bottlegrowth_split_mig_inbreeding optimization ' + str(i) + '*'*20)
+        popt, ll_model = dadi.Inference.opt(p0, fs, demo_model_ex, pts, upper_bound = upper, lower_bound = lower)
+        model_fs = demo_model_ex(popt, ns, pts)
+        theta0 = dadi.Inference.optimal_sfs_scaling(model_fs, fs)
+        res = [ll_model] + list(popt) + [theta0]
+        fid.write('\t'.join([str(ele) for ele in res]) + '\n')
+        print('Finished bottlegrowth_split_mig_inbreeding optimization ' + str(i) + '*'*20)
+    fid.close()
+def main():
+    data_fs = dadi.Spectrum.from_file('FRA_GER_syn_unfolded.fs')
+    ns = data_fs.sample_sizes
+    pts_l = [max(ns)+20, max(ns)+30, max(ns)+40]
+
+    bottlegrowth_split_mig_demography(data_fs, ns, pts_l)
+if __name__ == '__main__':
+    main()
