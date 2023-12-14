ad08f53a5f525d800ec2360e94d78527a0c98176
oliviafernflores <olivia@Olivias-MacBook-Pro.local>
2023-12-13 18:31:15 -0700

    updates to FRA vs. IRA

--- FRA_IRA/plot_dfe.py
+++ FRA_IRA/plot_dfe.py
@@ -10,10 +10,8 @@ def get_params(fname):
     fp = open(fname, 'r')
     lines = fp.readlines()
     best_params = lines[0].strip('\n').split('\t')
-    print(best_params)
     for line in lines:
         log_likelihood = str(line.strip('\n').split('\t')[0].strip('-'))
-        print(log_likelihood)
         if float(log_likelihood) < float(best_params[0].strip('-')):
             best_params = line.strip('\n').split('\t')
     popt = best_params[1:-1]
@@ -69,17 +67,17 @@ def main():
     theta0 = 26966.22426449829
     theta_ns = theta0 * 2.4
     
-    # popt_1d, ll_1d  = get_params('mmd_FRA_mmd_IRA_1D_lognormal_DFE.txt')
-    # plot_lognormal_dfe(popt_1d, dfe_func1d, sele_dist1d, theta_ns, data_fs, 'mmd_FRA_mmd_IRA_1D_lognormal_DFE')
-    # print('\n')
-    # print('1d lognormal best fit params: ' + str(popt_1d) + ' with log likelihood: ' + str(ll_1d))
-    # print('\n')
+    popt_1d, ll_1d  = get_params('mmd_FRA_mmd_IRA_1D_lognormal_DFE.txt')
+    plot_lognormal_dfe(popt_1d, dfe_func1d, sele_dist1d, theta_ns, data_fs, 'mmd_FRA_mmd_IRA_1D_lognormal_DFE')
+    print('\n')
+    print('1d lognormal best fit params: ' + str(popt_1d) + ' with log likelihood: ' + str(ll_1d))
+    print('\n')
 
-    # popt_2d, ll_2d = get_params('mmd_FRA_mmd_IRA_2D_biv_lognormal_DFE.txt')
-    # plot_biv_lognormal_dfe(popt_2d, dfe_func2d, sele_dist2d, theta_ns, data_fs, 'mmd_FRA_mmd_IRA_2D_biv_lognormal_DFE')
-    # print('\n')
-    # print('2d bivariate lognormal best fit params: ' + str(popt_2d)+ ' with log likelihood: ' + str(ll_2d))
-    # print('\n')
+    popt_2d, ll_2d = get_params('mmd_FRA_mmd_IRA_2D_biv_lognormal_DFE.txt')
+    plot_biv_lognormal_dfe(popt_2d, dfe_func2d, sele_dist2d, theta_ns, data_fs, 'mmd_FRA_mmd_IRA_2D_biv_lognormal_DFE')
+    print('\n')
+    print('2d bivariate lognormal best fit params: ' + str(popt_2d)+ ' with log likelihood: ' + str(ll_2d))
+    print('\n')
 
     popt_mixture, ll_mix = get_params('mmd_FRA_mmd_IRA_mixture_DFE.txt')
     plot_mixture_simple_dfe(popt_mixture, cache1d, cache2d, sele_dist1d, sele_dist2d, theta_ns, data_fs, 'mmd_FRA_mmd_IRA_simple_mixture_DFE')
@@ -89,7 +87,7 @@ def main():
     
     popt_gamma, ll_gamma = get_params('mmd_FRA_mmd_IRA_gamma_DFE.txt')
     gamma_dist = DFE.PDFs.gamma
-    plot_gamma_dfe(popt_gamma, dfe_func1d, gamma_dist, theta_ns, data_fs, 'mmd_FRA_mmd_IRA_gammal_DFE')
+    plot_gamma_dfe(popt_gamma, dfe_func1d, gamma_dist, theta_ns, data_fs, 'mmd_FRA_mmd_IRA_gamma_DFE')
     print('\n')
     print('gamma with shared selection best fit params: ' + str(popt_gamma) + ' with log likelihood: ' + str(ll_gamma))
     print('\n')
