7ea31d0dd38f1d956347bd234ff65c863feea7bc
oliviafernflores <oliviafernflores@arizona.edu>
2024-08-05 14:28:27 -0700

    updates

--- new_demography/IRA_FRA/mmd_FRA_mmd_IRA_2d_demographics_im_pre_inbreeding.py
+++ new_demography/IRA_FRA/mmd_FRA_mmd_IRA_2d_demographics_im_pre_inbreeding.py
@@ -66,8 +66,8 @@ def im_pre_demography(fs, ns, pts):
     demo_model = dadi.Numerics.make_anc_state_misid_func(demo_model)
     demo_model_ex = dadi.Numerics.make_extrap_func(demo_model)
     params = [1, 0.1, 0.1, 1, 1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
-    lower = [1e-2, 1e-2, 1e-5, 1e-2, 1e-2, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 0]
-    upper = [3, 1, 1, 3, 100, 10, 3, 1, 1, 1, 1]
+    lower = [1e-2, 1e-2, 1e-5, 1e-2, 1e-2, 1e-3, 1e-3, 1e-3, 1e-5, 1e-5, 1e-5]
+    upper = [10, 1, 1, 3, 100, 10, 3, 1, 1, 1, 1]
     try:
         fid = open(f'demo_results/IRA_FRA_im_pre_inbreeding_demo_fits{process_ii}.txt', 'a')
     except:
