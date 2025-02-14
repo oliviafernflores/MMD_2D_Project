72615326362fcab9e985a74456dcbcff678b8279
oliviafernflores <olivia@Olivias-MacBook-Pro.local>
2025-02-14 07:05:10 -0700

    dadi-cli

--- uncertainties_bad_error_dadi_cli/test_uncertainties.sh
+++ uncertainties_bad_error_dadi_cli/test_uncertainties.sh
@@ -1,32 +1,13 @@
 boots_nsyn_dir="/Users/olivia/Documents/GO_0009987_nsyn_bootstraps"
-boots_syn_dir="IRA_FRA_syn_bootstraps"
+boots_syn_dir="/Users/olivia/Documents/IRA_FRA_syn_bootstraps"
 fs_file="0009987_Mmd_FRA_Mmd_IRA_SFS_nsyn_unfolded.fs"
 cache2d_file="mmd_FRA_mmd_IRA_2d_cache.bpkl"
 cache1d_file="mmd_FRA_mmd_IRA_1d_cache.bpkl"
 
 
 # Lognormal
-# Error: None!
+# Error: None, should be because paths to bootstraps do not exist. Incorrect path for fs or cache(s) does raise error.
 dfe_popt_file="mmd_FRA_mmd_IRA_1d_lognormal_DFE_0009987.InferDFE.bestfits"
 output_file="mmd_FRA_mmd_IRA_1d_lognormal_DFE_0009987.InferDFE.godambe"
 dadi-cli StatDFE --bootstrapping-nonsynonymous-dir "$boots_nsyn_dir" --bootstrapping-synonymous-dir "$boots_syn_dir" --fs "$fs_file" --pdf2d lognormal --dfe-popt "$dfe_popt_file" --cache1d "$cache1d_file" --output "$output_file"
 
-
-# Symmetric Bivariate Independent Gamma
-# Error: None!
-# dfe_popt_file="mmd_FRA_mmd_IRA_2d_biv_ind_gamma_symmetric_DFE_0009987.InferDFE.bestfits"
-# output_file="mmd_FRA_mmd_IRA_2d_biv_ind_gamma_symmetric_DFE_0009987.InferDFE.godambe"
-# dadi-cli StatDFE --bootstrapping-nonsynonymous-dir "$boots_nsyn_dir" --bootstrapping-synonymous-dir "$boots_syn_dir" --fs "$fs_file" --pdf2d biv_ind_gamma --dfe-popt "$dfe_popt_file" --cache2d "$cache2d_file" --output "$output_file"
-
-
-# Asymmetric Bivariate Independent Gamma
-# Error: None!
-# dfe_popt_file="mmd_FRA_mmd_IRA_2d_biv_ind_gamma_asymmetric_DFE_0009987.InferDFE.bestfits"
-# output_file="mmd_FRA_mmd_IRA_2d_biv_ind_gamma_asymmetric_DFE_0009987.InferDFE.godambe"
-# dadi-cli StatDFE --bootstrapping-nonsynonymous-dir "$boots_nsyn_dir" --bootstrapping-synonymous-dir "$boots_syn_dir" --fs "$fs_file" --pdf2d biv_ind_gamma --dfe-popt "$dfe_popt_file" --cache2d "$cache2d_file" --output "$output_file"
-
-# Gamma
-# Error: None!
-# dfe_popt_file="mmd_FRA_mmd_IRA_2d_gamma_DFE_0009987.InferDFE.bestfits"
-# output_file="mmd_FRA_mmd_IRA_2d_gamma_DFE_0009987.InferDFE.godambe"
-# dadi-cli StatDFE --bootstrapping-nonsynonymous-dir "$boots_nsyn_dir" --bootstrapping-synonymous-dir "$boots_syn_dir" --fs "$fs_file" --pdf2d gamma --dfe-popt "$dfe_popt_file" --cache1d "$cache1d_file" --output "$output_file"
